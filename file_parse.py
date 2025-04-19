3369a2899b8cc696a90c8329ede96af65b222d34
Kelley <kelley_tu@brown.edu>
2025-04-19 17:17:08 -0400

    .

--- file_parse.py
+++ file_parse.py
@@ -1,28 +1,29 @@
 from pathlib import Path
 
+
 def parse_file(filepath: str):
-    used_orgs = set()
+    used_orgs = set() # hashset storing the IDs of organisms that have already been included
 
     output_dir = Path.cwd() / "id_lists"
     output_dir.mkdir(exist_ok=True)
 
-    pfam_id = filepath.name.split(".")[0]
-    pdb_filepath = output_dir / ("pbd_list_" + pfam_id + ".txt")
+    pfam_id = filepath.name.split(".")[0] # PFAM ID of the organism
+    pdb_filepath = output_dir / (pfam_id + "_id_list.txt") # filepath of the new id file
     
     with open(filepath, 'r') as stockholm, open(pdb_filepath, 'a') as output:
         for line in stockholm:
-            if "_HUMAN" in line and "DR PDB;" in line:
+            if "_HUMAN" in line and "DR PDB;" in line: # only use human examples with existing PDB IDs
                 org = line.split()[1].split("/")[1]
-                pbd_id = line.split()[4]
-                if org not in used_orgs:
+                pdb_id = line.split()[4]
+                if org not in used_orgs: # if not already included, add data to file
                     used_orgs.add(org)
                     with open(pdb_filepath, "a") as file:
-                        file.write(pbd_id + ";" + org + "\n")
+                        file.write(pdb_id + ";" + org + "\n")
 
-file_num = 1
-for data_file in (Path.cwd() / "stockholm_data").iterdir():
+for id_file in (Path.cwd() / "id_lists").iterdir(): # remove existing files from the id_lists folder
+    Path.unlink(id_file)
+for data_file in (Path.cwd() / "stockholm_data").iterdir(): # add new id files to the id_lists folder
     parse_file(data_file)
-    file_num += 1
 
 
 
