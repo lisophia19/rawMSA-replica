6a47e4a80f9c5af0e2d229067e28f5d1fc4e3363
Sophia Li <sophia_y_li1@brown.edu>
2025-04-19 11:25:42 -0400

    model script

---
+++ model_conversion.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import os
+import tensorflow as tf
+print(tf.__version__)
+
+#run from scripts folder
+with open('../models/ss/list', 'r') as file:
+    model_list = file.read().splitlines()
+
+print(model_list[:2])
+
+for model_path in model_list[:1]:
+    model_path = "../models/ss/" + model_path[10:]
+    model = tf.keras.models.load_model(model_path)
+    model.summary()
+
+    #get more information abt layer parameters
+    for layer in model.layers:
+        print(layer.name, layer.__class__.__name__, layer.get_config())
+
