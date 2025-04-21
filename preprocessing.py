1f0f9765e01cbf0f49728ffde080e4e8fe3faef9
Ethan Ye <ethan.ye0312@gmail.com>
2025-04-21 19:03:11 -0400

    Finished preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -1,47 +1,97 @@
-from Bio import SeqIO
+# import tensorflow as tf
+import numpy as np
 from pathlib import Path
 
 letter_to_number = { 'P':1, 'U':2, 'C':3, 'A':4, 'G':5, 'S':6, 'N':7, 'B':8, 'D':9, 'E':10, 'Z':11, 'Q':12, 'R':13, 'K':14, 'H':15, 'F':16, 'Y':17, 'W':18, 'M':19, 'L':20, 'I':21, 'V':22, 'T':23, '.':24, 'X':25 }
+ss_to_number = {'H': 1, 'E':2, 'T': 3, 'S': 4, 'G': 5, 'I':6, 'C':7, '.':8, '-':9}
 
+# def preprocess_data(stockholm_file : str):
+#     output_dir = Path.cwd() / "fasta_data"
+#     output_dir.mkdir(exist_ok=True)
 
-def preprocess_data(stockholm_file : str):
-    output_dir = Path.cwd() / "fasta_data"
-    output_dir.mkdir(exist_ok=True)
+#     pfam_id = stockholm_file.name.split(".")[0] # PFAM ID of the organism
+#     output_filepath = output_dir / (pfam_id + "_fasta") # filepath of the new file
 
-    pfam_id = stockholm_file.name.split(".")[0] # PFAM ID of the organism
-    output_filepath = output_dir / (pfam_id + "_fasta") # filepath of the new file
+#     # fasta to integer:
+#     map_to_integer(output_filepath)
 
-    # fasta to integer:
-    map_to_integer(output_filepath)
+def compile_tensor(line, sequence_type):
+    line = line.rstrip()
+    
+    count = 0
+    # Represents the number of residues 
+    limit = 200
+
+    current_seq = np.zeros(limit)
+
+    line_len = len(line)
+
+    while count < limit:
+        if count >= line_len:
+            current_seq[count] = 25 if sequence_type == 'SEQUENCE' else 10
+        else:
+            try:
+                number = letter_to_number[line[count]] if sequence_type == 'SEQUENCE' else ss_to_number[line[count]]
+            except KeyError:
+                number = 25 if sequence_type == 'SEQUENCE' else 10
+
+            current_seq[count] = number
+        count += 1
+
+    return current_seq
+
 
 def map_to_integer(data_file : str):
-    count = 0
-    limit = 3000
     
-    output_dir = Path.cwd() / "preprocessed_data"
-    output_dir.mkdir(exist_ok=True)
+    all_sequences = []
+    sequence_labels = []
 
-    output_filepath = output_dir / "preprocessed_data" # filepath of the new file
-
-    with open(data_file, "r") as data_file, open(output_filepath, "a") as output_file:
-        for line in data_file:
-
-            if count == limit:
-                break
-
+    sequence_type = 'SEQUENCE'
+    with open(data_file, 'r') as unprocessed_data:
+        for line in unprocessed_data:
             if line.startswith('>'):
-                output_file.write("\n" + line)
+                sequence_type = 'SEQUENCE'
+                print("SEQUENCE LINE: ", line)
+                continue
+            if line.startswith('<'):
+                sequence_type = 'LABEL'
+                print("LABEL LINE: ", line)
                 continue
 
-            index = 0
-            integers = []
-            while index < len(line.rstrip()):
-                try:
-                    number = letter_to_number[line[index]]
-                    integers.append(str(number))
-                except KeyError:
-                    number = 25      
-                index += 1
-            output_file.write(" ".join(integers))
+            if sequence_type == 'SEQUENCE':
+                compiled_seq = compile_tensor(line, sequence_type)
+                all_sequences.append(compiled_seq)
+            else:
+                compiled_seq = compile_tensor(line, sequence_type)
+                sequence_labels.append(compiled_seq)
+    
+    return np.array(all_sequences), np.array(sequence_labels)
 
-map_to_integer(Path.cwd() / "fasta-pbd_list_PF00071.txt")
+print(map_to_integer(Path.cwd() / "fasta-pbd_list_PF00071.txt"))
+
+    # output_dir = Path.cwd() / "preprocessed_data"
+    # output_dir.mkdir(exist_ok=True)
+
+    # output_filepath = output_dir / "preprocessed_data" # filepath of the new file
+
+    # with open(data_file, "r") as data_file, open(output_filepath, "a") as output_file:
+    #     for line in data_file:
+
+    #         if count == limit:
+    #             break
+
+    #         if line.startswith('>'):
+    #             output_file.write("\n" + line)
+    #             continue
+
+    #         index = 0
+    #         integers = []
+    #         while index < len(line.rstrip()):
+    #             try:
+    #                 number = letter_to_number[line[index]]
+    #                 integers.append(str(number))
+    #             except KeyError:
+    #                 number = 25      
+    #             index += 1
+    #         output_file.write(" ".join(integers))
+
