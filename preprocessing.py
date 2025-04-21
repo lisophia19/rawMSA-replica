1b138234582d7409abef1bbd3161e878f6240caa
Kelley <kelley_tu@brown.edu>
2025-04-21 19:16:36 -0400

    .

--- preprocessing.py
+++ preprocessing.py
@@ -5,16 +5,6 @@ from pathlib import Path
 letter_to_number = { 'P':1, 'U':2, 'C':3, 'A':4, 'G':5, 'S':6, 'N':7, 'B':8, 'D':9, 'E':10, 'Z':11, 'Q':12, 'R':13, 'K':14, 'H':15, 'F':16, 'Y':17, 'W':18, 'M':19, 'L':20, 'I':21, 'V':22, 'T':23, '.':24, 'X':25 }
 ss_to_number = {'H': 1, 'E':2, 'T': 3, 'S': 4, 'G': 5, 'I':6, 'C':7, '.':8, '-':9}
 
-# def preprocess_data(stockholm_file : str):
-#     output_dir = Path.cwd() / "fasta_data"
-#     output_dir.mkdir(exist_ok=True)
-
-#     pfam_id = stockholm_file.name.split(".")[0] # PFAM ID of the organism
-#     output_filepath = output_dir / (pfam_id + "_fasta") # filepath of the new file
-
-#     # fasta to integer:
-#     map_to_integer(output_filepath)
-
 def compile_tensor(line, sequence_type):
     line = line.rstrip()
     
